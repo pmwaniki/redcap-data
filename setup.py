db3b50bc08cd76dec3134fd4e90cf1c7b26563bd
Paul Mwaniki <pmmwaniki06@gmail.com>
2023-05-01 10:59:43 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Measurements',
+    url='https://github.com/jladan/package_demo',
+    author='Paul Mwaniki',
+    author_email='pmmwaniki06@gmail.com',
+    # Needed to actually package something
+    packages=['datasets'],
+    # Needed for dependencies
+    install_requires=['numpy','pandas','grequests','requests'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Import data from redcap',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
